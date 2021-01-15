9e276362d6085394846206da82e5b8766ff22bb3
Daniel Robertson <danieljrobertson@gmail.com>
2021-01-15 16:21:07 -0500

    unpublished episodes are not rendered

--- src/Components/Helpers/ParseEpisodes.js
+++ src/Components/Helpers/ParseEpisodes.js
@@ -1,16 +1,19 @@
-import { format } from 'date-fns'
+import { format, isFuture } from 'date-fns'
 
 export default function parseEpisodes(episodes) { 
-  const parsedEpisodes = episodes.map(episode => {
-    const ep = {
-      id: episode.id,
-      title: episode.title,
-      description: {__html: episode.description},
-      published_at: `Published on ${format(new Date(episode.published_at), "PPPP")}`,
-      artwork_url: episode.artwork_url,
-      audio_url: episode.audio_url
-    }
-    return ep
+  const parsedEpisodes = episodes
+              .filter(episode => !isFuture(new Date(episode.published_at)))
+              .map(episode => {
+      const ep = {
+        id: episode.id,
+        title: episode.title,
+        description: { __html: episode.description },
+        published_at: `Published on ${format(new Date(episode.published_at), "PPPP")}`,
+        artwork_url: episode.artwork_url,
+        audio_url: episode.audio_url
+      }
+      return ep
+  
   })
   return parsedEpisodes;
 }
