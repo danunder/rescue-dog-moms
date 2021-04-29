001a4d06da0257cb82488e6760455870903f88b6
Daniel Robertson <danieljrobertson@gmail.com>
2021-04-29 12:36:12 -0400

    update About page text

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,16 +14,16 @@ function About() {
         <img src={YamandBoss} alt="Podcast creator and her fur baby" />
       </div>
       <p>
-        <strong>About Yamini:</strong> Yamini is a public relations professional and a rescue dog mom. She spends her free time volunteering and fostering with various Toronto dog rescues, including Fetch + Releash and New Collar Collective. She loves reality TV, the Internet (especially TikTok right now) and eating delicious food. Sometimes she shares it with Boss.<br/><br/>
+        <strong>About Yamini:</strong> Yamini is a public relations professional and a rescue dog mom. She spends her free time volunteering and fostering with various Toronto dog rescues. She loves reality TV, the Internet (especially TikTok right now) and eating delicious food. Sometimes she shares it with Boss.<br/><br/>
 
       <strong>About Boss:</strong> Boss is a rescue dog from Tennessee, rescued by Fetch + Releash. He was adopted in May 2020 and in this short time of being a pandemic baby, he has already conquered so many hearts. His favourite things are squeaky balls, baby carrots and blankets. <br/><br/>
 
-      <strong>Want to adopt or foster in the GTA?</strong> It is important you do your research on which rescue you do that through. Check out these RDM approved rescues:</p>
-      <ul>
+      <strong>Want to adopt or foster in the GTA?</strong> It is important you do your research on which rescue you do that through.</p>
+      {/* <ul>
         <li><a href="https://www.fetchandreleash.ca/" rel="noreferrer" target="_blank">Fetch + Releash</a></li>
         <li><a href="https://newcollar.co/" rel="noreferrer" target="_blank">New Collar Collective</a></li>
         <li><a href="https://www.straytoplay.org/" rel="noreferrer" target="_blank">Stray to Play</a></li>
-      </ul>
+      </ul> */}
       <p>Want to contact us? DM us on Instagram (<a href="https://www.instagram.com/rescuedogmomspod/" rel="noreferrer" target="_blank">@rescuedogmomspodcast</a>) or email me @ <a href="mailto:rescuedogmoms@gmail.com" rel="noreferrer" target="_blank">rescuedogmoms@gmail.com</a></p>
 
 
